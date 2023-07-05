191d191fef1831b3327249f4b724ed448d53c307
ShueiYang <yangshuei83@gmail.com>
2023-07-05 18:56:08 +0200

    prettier style

--- src/components/themeContext/ThemeProvider.tsx
+++ src/components/themeContext/ThemeProvider.tsx
@@ -6,35 +6,35 @@ type ThemeColor = "light" | "dark";
 
 //create context for theme color to be used in other components
 export const ThemeContext = createContext(
-    {} as { 
-        theme: ThemeColor, 
-        setTheme: (theme: ThemeColor) => void 
-    }
+  {} as { 
+    theme: ThemeColor, 
+    setTheme: (theme: ThemeColor) => void 
+  }
 );
+
 
 function ThemeProvider({
   children
 }: {
   children: React.ReactNode
 }) {
-    const [ theme, setTheme ] = useState(()=> {
-
-        //check if Server Side Rendering
-        if (typeof window === "undefined") {
-            return "light";
-        }
-        if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
-            return localStorage.getItem("theme") as ThemeColor;
-        }
-        if (!localStorage && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            return "dark"
-        } 
+  const [ theme, setTheme ] = useState(()=> {
+    //check if Server Side Rendering
+    if (typeof window === "undefined") {
         return "light";
-    });
+    }
+    if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+        return localStorage.getItem("theme") as ThemeColor;
+    }
+    if (!localStorage && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark"
+    } 
+    return "light";
+  });
     
   return (
     <ThemeContext.Provider value={{theme, setTheme}}>
-        {children}
+      {children}
     </ThemeContext.Provider>
   )
 }
