993277d7752484dd8eb71aa6238080bad9b61db4
ShueiYang <yangshuei83@gmail.com>
2023-07-10 12:50:09 +0200

    update middleware to fix implicite return

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,40 +5,40 @@ import { verifyAuth } from "./lib/auth";
 export async function middleware(req: NextRequest) {
 
   const jwtCookie = req.cookies.get("shueiJWT") 
-  const jwtSecret = process.env.JWT_SECRET
+  const jwtSecret = process.env.JWT_SECRET || "";
   const url = req.url;
-  
-  if(!jwtCookie && url.includes("/backoffice/dashboard")) {
-    return NextResponse.redirect(new URL("/backoffice/admin", url)) 
-  }
-  if(!jwtCookie && req.nextUrl.pathname.startsWith("/api/auth")) {
-    return NextResponse.json(
-      { message: "Authentification required"},
-      { status: 401 }     
-    );
-  }
-  try {
-    if(jwtCookie && jwtSecret) {
-      const tokenValue = jwtCookie.value;
-      // verify if the jwt is valid
-      await verifyAuth(tokenValue, jwtSecret);  
 
-      if(url.includes("/backoffice/admin")) {
-        return NextResponse.rewrite(new URL("/backoffice/dashboard", url))
-      }
-      return NextResponse.next();
-    }            
+  if(!jwtCookie) {
+    if(url.includes("/backoffice/admin")) {
+      return;
+    } else if (url.includes("/backoffice/dashboard")) {
+      return NextResponse.redirect(new URL("/backoffice/admin", url)) 
+    } else {
+      return NextResponse.json(
+        { message: "Authentification required"},
+        { status: 401 }     
+      )
+    }
+  };
+  try {
+    const tokenValue = jwtCookie.value;
+    // verify if the jwt is valid
+    await verifyAuth(tokenValue, jwtSecret);  
+
+    if(url.includes("/backoffice/admin")) {
+      return NextResponse.rewrite(new URL("/backoffice/dashboard", url))
+    }
+    return NextResponse.next();
+               
   } catch (err) {
     console.error(err);
-    if(url.includes("/backoffice/dashboard")) {
+    if(req.nextUrl.pathname.startsWith("/backoffice/dashboard")) {
       return NextResponse.redirect(new URL("/backoffice/admin", url)) 
     } 
-    if(req.nextUrl.pathname.startsWith("/api/auth")) {
-      return NextResponse.json(
-        { message: "Jwt is expired or is not valid"},
-        { status: 403 }     
-      );
-    } 
+    return NextResponse.json(
+      { message: "Jwt is expired or is not valid"},
+      { status: 403 }     
+    );   
   }   
 }
  
