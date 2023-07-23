3797557a27887799d1c00d4c95e03b4f87ed2f11
ShueiYang <yangshuei83@gmail.com>
2023-07-23 16:44:57 +0200

    reformat and add prefetch property to Link

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -1,26 +1,33 @@
 import NextLink from "next/link";
 
 // function to pick Link component or a Html tag depending the route name
-export function getLinkAndProps (route: string,) {
-    const Link = (
-        route === "/projects" || 
-        route === "/contact" || 
-        route === "/backoffice/admin"        
-    ) ? NextLink : "a"
+export function getLinkAndProps(route: string) {
+  const Link =
+    route === "/projects" ||
+    route === "/contact" ||
+    route === "/backoffice/admin"
+      ? NextLink
+      : "a";
 
-    const linkProps = Link === "a" ? {rel:"noopener noreferrer", target: "_blank"} : {scroll: false};
-    return {
-        Link,
-        linkProps
-    }
+  const prefetchPath = route !== "/contact";
+
+  const linkProps =
+    Link === "a"
+      ? { rel: "noopener noreferrer", target: "_blank" }
+      : { prefetch: prefetchPath, scroll: false };
+
+  return {
+    Link,
+    linkProps,
+  };
 }
 
 export function scrollAfterLoad() {
-    if (typeof window !== "undefined") {
-        window.scrollTo(0, 0);
-    }
+  if (typeof window !== "undefined") {
+    window.scrollTo(0, 0);
+  }
 }
 
 export function easeOutCirc(x: number) {
-    return Math.sqrt(1 - Math.pow(x - 1, 4))
+  return Math.sqrt(1 - Math.pow(x - 1, 4));
 }
