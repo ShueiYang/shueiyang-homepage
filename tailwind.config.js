55412e8e390782757bd2f4279e1a997db162fb46
ShueiYang <yangshuei83@gmail.com>
2023-07-30 15:41:58 +0200

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,35 +2,35 @@
 
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   
   theme: {
     container: {
       center: true,
       screens: {
-        'sm': '640px', 
-        'md': '768px', 
-        'lg': '1024px',
-        'xl': '1280px',
+        "sm": "640px", 
+        "md": "768px", 
+        "lg": "1024px",
+        "xl": "1280px",
       },
     },
     extend: {
       fontFamily: {
-        'ibm': ['var(--font-ibm)'],
-        'robo': ['var(--font-robo)']
+        "ibm": ["var(--font-ibm)"],
+        "robo": ["var(--font-robo)"]
       },
       colors: {
-        'sand-l': 'rgb(237, 143, 3, 0.4)', 
-        'grn-l': 'rgb(220, 227, 91, 0.4)', 
-        'black-russ': '#111827',
-        'purp-d': 'rgb(167, 112, 239, 0.4)',
-        'sea-d': 'rgb(41, 128, 185, 0.4)',
-        'blackhover': 'rgba(0,0,0,0.6)'
+        "sand-l": "rgb(237, 143, 3, 0.4)", 
+        "grn-l": "rgb(220, 227, 91, 0.4)", 
+        "black-russ": "#111827",
+        "purp-d": "rgb(167, 112, 239, 0.4)",
+        "sea-d": "rgb(41, 128, 185, 0.4)",
+        "blackhover": "rgba(0,0,0,0.6)"
       },
       boxShadow : {
-        custom: '3px 6px 6px 1px rgba(0,0,0,0.5)',
+        custom: "3px 6px 6px 1px rgba(0,0,0,0.5)",
       }
     }
   },
