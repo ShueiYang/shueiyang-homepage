c287020c4f97de0f751e8747f4b63284c9fee5f3
ShueiYang <yangshuei83@gmail.com>
2023-08-19 14:23:55 +0200

    refactor convertToFormData

---
+++ src/utils/formDataHelper.ts
@@ -0,0 +1,33 @@
+import { ProjectForm } from "@root/common.types";
+import { FieldsProps } from "@/components/ProjectForm";
+import { processImage } from "./imageTool";
+
+
+
+export async function convertRawDataToFormData(
+  data: ProjectForm,
+  dirtyFields: FieldsProps
+): Promise<FormData> {
+
+  const formData = new FormData();
+  const formField = Object.entries(data);
+
+  await Promise.all(
+    formField.map(async ([key, value]) => {
+      //check if the value is a FileList instance and if there is a file in FileList.
+      if (key === "imageFile") {
+        if (value instanceof FileList && value.length) {
+          const result = await processImage(value[0]);
+          formData.append(key, result);
+        }
+      } else {
+      // only append data if is modified in the input value
+        if(dirtyFields[key as keyof ProjectForm]) {
+          formData.append(key, value);
+        }
+      }
+      return null;
+    })
+  )
+  return formData;
+} 
